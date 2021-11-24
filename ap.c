c0f523ac5eeede7473b78f725ddf240f40c0d0f5
Pedro Cunha <phcs-12@hotmai.com>
2021-11-24 17:12:51 -0300

    Fake final!

--- ap.c
+++ ap.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <windows.h>
 
 // Globais
 typedef struct node{
@@ -18,13 +19,47 @@ int check(int n);
 int addL1(int n);
 int addL2(int n);
 int addL3(int n);
-int del();
+int del(void);
 void joinLists(void);
+void loading(void);
 
 int main(){
+    int r = 0;
+    int count = 0;
+    int total = 0;
+    int itemDeleted = 0;
 
+    while(r != -1){
+        system("cls");
+        printf("Digite os inteiros do conjunto A: (digite -1 para ir ao proximo conjunto)\n");
+        scanf("%d",&r);
+        if(r > 0) addL1(r);
+    }
+    r = 0;
+    while(r != -1){
+        system("cls");
+        printf("Digite os inteiros do conjunto B: (digite -1 para ir ao proximo conjunto)\n");
+        scanf("%d",&r);
+        if(r > 0) addL2(r);
+    }
     joinLists();
+    loading();
 
+    printf("- Elementos do conjunto C -\n");
+    while(l3 != NULL){
+        itemDeleted = del();
+        if(itemDeleted != -1){
+            printf("%d, ", itemDeleted);
+            total += itemDeleted; // Somatório dos itens
+            count++; // Contagem dos itens
+        }
+    }
+
+    total = total / count; // Cáculo da média
+
+    printf("\n Media do conjunto C => %d", total);
+
+    getch();
     return 0;
 }
 
@@ -125,6 +160,7 @@ int addL3(int n){
         //Primeiro item da lista
     }
     
+
     Node *lastNode = l3;
     while(lastNode->link != NULL){
         lastNode = lastNode->link;
@@ -135,35 +171,57 @@ int addL3(int n){
 
 int del(){
     Node *temp;
+    Node *prev;
+    Node *next;
     int smaller;
 
+    // Caso a lista esteja vazia
     if(l3 == NULL) return -1;
 
     smaller = l3->item;
     temp = l3;
     
+    if(l3->link == NULL){
+        // Verifica se há apenas 1 item na lista para retornar este único item e deixar a lista vazia 
+        l3 == NULL;
+        return smaller;
+    }
+
+    // Verifica qual o elemento de menor valor
     while(temp->link != NULL){
         if(temp->item < smaller){
             smaller = temp->item;
+            next = temp->link;
         }
         temp = temp->link;
     }
 
+   // Retira o nó pertencente ao menor valor da lista 
+    temp = l3;
+    while(temp->link != NULL){
+        if(temp->item == smaller){
+            prev->link = next;
+        }
+        prev = temp;
+        temp = temp->link;
+    }
+    temp = NULL;
     return smaller;
 }
 
 void joinLists(void){
     Node *temp;
     int r;
-    // l3 = (Node *)malloc(sizeof(Node));
-    // l3 -> item = NULL;
-    // l3 -> link = NULL;
+
+    // Verifica a lista 1
     temp = l1;
     while(temp->link != NULL){
         r = check(temp->item);
         if(r != 3) addL3(temp->item);
         temp = temp->link;
     }
+
+    // Verifica a lista 2
     temp = l2;
     while(temp->link != NULL){
         r = check(temp->item);
@@ -172,6 +230,42 @@ void joinLists(void){
     }
 }
 
+void loading(){
+    system("cls");
+    printf("Gerando conjunto C  [          ]");
+    Sleep(1000);
+    system("cls");
+    printf("Gerando conjunto C  [=         ]");
+    Sleep(1000);
+    system("cls");
+    printf("Gerando conjunto C  [==        ]");
+    Sleep(1000);
+    system("cls");
+    printf("Gerando conjunto C  [===       ]");
+    Sleep(800);
+    system("cls");
+    printf("Gerando conjunto C  [====      ]");
+    Sleep(600);
+    system("cls");
+    printf("Gerando conjunto C  [=====     ]");
+    Sleep(1000);
+    system("cls");
+    printf("Gerando conjunto C  [======    ]");
+    Sleep(1000);
+    system("cls");
+    printf("Gerando conjunto C  [=======   ]");
+    Sleep(500);
+    system("cls");
+    printf("Gerando conjunto C  [========  ]");
+    Sleep(1000);
+    system("cls");
+    printf("Gerando conjunto C  [========= ]");
+    Sleep(1000);
+    system("cls");
+    printf("Gerando conjunto C  [==========]");
+    Sleep(1000);
+    system("cls");
+}
 // int getch(void){ // Implementação getch para linux
 //     struct termios oldattr, newattr;
 //     int ch;
